3a61541e782a340216da552def21ebd4ceb40824
Matheus Afonso <matheusafonsouza@gmail.com>
2023-12-11 22:11:43 -0300

    pocs communication with http adapter
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

---
+++ adapters/http.py
@@ -0,0 +1,52 @@
+import requests
+
+
+class HTTPRequestAdapter:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def request(self, method, route, data=None):
+        try:
+            response = requests.request(
+                method,
+                f"{self.base_url}{route}",
+                data=data
+            )
+            response.raise_for_status()
+            return {
+                "status_code": response.status_code,
+                "content": response.json(),
+            }
+        except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.Timeout,
+            requests.exceptions.RequestException,
+        ) as e:
+            return {
+                "status_code": 500,
+                "content": {
+                    "message": str(e),
+                }
+            }
+        except requests.exceptions.HTTPError as e:
+            return {
+                "status_code": response.status_code,
+                "content": {
+                    "message": str(e),
+                }
+            }
+
+    def get(self, route):
+        return self.request("GET", route)
+    
+    def post(self, route, data):
+        return self.request("POST", route, data=data)
+    
+    def put(self, route, data):
+        return self.request("PUT", route, data=data)
+    
+    def delete(self, route):
+        return self.request("DELETE", route)
+    
+    def patch(self, route, data):
+        return self.request("PATCH", route, data=data)
---
+++ services/server3/main.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from adapters.http import HTTPRequestAdapter
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def ping():
+    return { "ping": True }
+
+
+@app.get("/ping-server-1")
+def ping_server_1():
+    response = HTTPRequestAdapter("http://server1:8000").get("/ping")
+    return JSONResponse(
+        status_code=response.get("status_code"),
+        content=response.get("content"),
+    )
+
+
+@app.get("/ping-server-2")
+def ping_server_2():
+    response = HTTPRequestAdapter("http://server2:8000").get("/ping")
+    return JSONResponse(
+        status_code=response.get("status_code"),
+        content=response.get("content"),
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
