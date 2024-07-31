9f81c74458caa705c88a5075d7c409623e01a39d
Thiago Carvalho <thiago099carvalho@gmail.com>
2024-07-30 21:48:05 -0300

    fix logging

--- common/amqp.py
+++ common/amqp.py
@@ -5,8 +5,8 @@ def get_rabbitmq_adapter(callback = None) -> RabbitMQAMQPAdapter:
     return RabbitMQAMQPAdapter(
         host="rabbitmq",
         port=5672,
-        username="test",
-        password="test",
+        username="guest",
+        password="guest",
         virtual_host="/",
         topic="test",
         callback=callback,
