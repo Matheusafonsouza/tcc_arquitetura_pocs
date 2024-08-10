9f5f425f981ccadd4d59ec7ce09de1df6f238824
Matheus Afonso <matheusafonsouza@gmail.com>
2024-08-10 20:10:40 -0300

    fix tests

--- tests/adapters/sql/test_postgres.py
+++ tests/adapters/sql/test_postgres.py
@@ -1,42 +1,39 @@
 from datetime import datetime
 
-from adapters.database.sql.postgres import users
+from adapters.database.sql.postgres import User
 
 
 def test_postgres_database_get_table(postgres_database):
     result = postgres_database.get_table("users")
-    assert result == users
+    assert result == User
 
 
 def test_postgres_database_create(postgres_database):
     result = postgres_database.create({"name": "test"})
-    assert result.id and isinstance(result.id, str)
-    assert result.created_at and isinstance(result.created_at, datetime)
-    assert result.updated_at and isinstance(result.updated_at, datetime)
-    assert result.name == "test"
+    assert result.get("id") and isinstance(result.get("id"), int)
+    assert result.get("created_at") and isinstance(result.get("created_at"), datetime)
+    assert result.get("updated_at") and isinstance(result.get("updated_at"), datetime)
+    assert result.get("name") == "test"
 
 
 def test_postgres_database_update(postgres_database):
-    user_id = postgres_database.create({"name": "test"}).id
+    user_id = postgres_database.create({"name": "test"}).get("id")
     result = postgres_database.update(user_id, {"name": "test123"})
-    assert result.id and isinstance(result.id, str)
-    assert result.created_at and isinstance(result.created_at, datetime)
-    assert result.updated_at and isinstance(result.updated_at, datetime)
-    assert result.name == "test123"
+    assert result == user_id
 
 
 def test_postgres_database_get(postgres_database):
-    user_id = postgres_database.create({"name": "test"}).id
+    user_id = postgres_database.create({"name": "test"}).get("id")
     result = postgres_database.get(user_id)
-    assert result.id and isinstance(result.id, str)
-    assert result.id == user_id
-    assert result.created_at and isinstance(result.created_at, datetime)
-    assert result.updated_at and isinstance(result.updated_at, datetime)
-    assert result.name == "test"
+    assert result.get("id") and isinstance(result.get("id"), int)
+    assert result.get("id") == user_id
+    assert result.get("created_at") and isinstance(result.get("created_at"), datetime)
+    assert result.get("updated_at") and isinstance(result.get("updated_at"), datetime)
+    assert result.get("name") == "test"
 
 
 def test_postgres_database_get(postgres_database):
-    user_id = postgres_database.create({"name": "test"}).id
+    user_id = postgres_database.create({"name": "test"}).get("id")
     postgres_database.delete(user_id)
     result = postgres_database.get(user_id)
     assert result == None
